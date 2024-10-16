c726407a4d4701a84760e3d9fc84431e08127f78
Жумаш Канат <zhumash.k@cdn.telecom.kz>
2024-10-16 23:11:34 +0500

    Добавлен файл 2_2.py

---
+++ 2_2.py
@@ -0,0 +1,84 @@
+# # print("Здравствуй, мир!")
+
+# print(4,8, 15, 16, 23, 42)
+
+# print(4)
+# print(8)
+# print(15)
+# print(16)
+# print(23)
+# print(42)
+
+# print("*")
+# print("**")
+# print("***")
+# print("****")
+# print("*****")
+# print("******")
+# print("*******")
+
+# print('Какой язык программирования ты изучаешь?')
+# language = input()
+# print(language, '- отличный выбор!')
+
+# name = input()
+# print("Привет," , name)
+# name = input()
+# print(name, "- чемпион!")
+
+# a = input()
+# b = input()
+# c = input()
+# print(a)
+# print(b)
+# print(c)
+
+# a = input()
+# b = input()
+# c = input()
+# print(c)
+# print(b)
+# print(a)
+
+# print('Mercury', 'Venus', sep='*', end='!')
+# print('Mars', 'Jupiter', sep='**', end='?')
+
+# sep1 = '#'
+# end2 = '?'
+# print('YES', 'NO', sep=sep1, end='+')
+# print('MAYBE', 'NEVER', sep='*', end=end2)
+
+# print('a', 'b', 'c', sep='*')
+# print('d', 'e', 'f', sep='**', end='')
+# print('g', 'h', 'i', sep='+', end='%')
+# print('j', 'k', 'l', sep='-', end='\n')
+# print('m', 'n', 'o', sep='/', end='!')
+# print('p', 'q', 'r', sep='1', end='%')
+# print('s', 't', 'u', sep='&', end='\n')
+# print('v', 'w', 'x', sep='%')
+# print('y', 'z', sep='/', end='!')
+
+
+# print('1БЕСКОНЕЧНОСТЬ', 'НЕ', 'ПРЕДЕЛ', '!', sep='_')
+
+# print('2БЕСКОНЕЧНОСТЬ_НЕ_ПРЕДЕЛ!')
+
+# print('3БЕСКОНЕЧНОСТЬ', 'НЕ', 'ПРЕДЕЛ!', sep='_', end='!')
+
+# print('4БЕСКОНЕЧНОСТЬ', 'НЕ', 'ПРЕДЕЛ', sep='_', end='')
+# print('!')
+
+# print('5БЕСКОНЕЧНОСТЬ_НЕ_ПРЕДЕЛ', end='')
+# print('!')
+
+# print('6БЕСКОНЕЧНОСТЬ_', 'НЕ_', 'ПРЕДЕЛ!', sep='', end='')
+
+# # put your python code here
+# print('I','like', 'Python', sep='***')
+
+# name = input()
+# print('Привет,', name, end='!')
+
+
+
+
